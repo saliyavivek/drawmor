c834f84df6ad11c80389f29526b09b8508300195
Vivek Saliya <viveksaliya004@gmail.com>
2025-06-13 17:43:28 +0530

    remove: export authOptions from /app/api/auth/[...nextauth]/route.ts

--- apps/web/app/api/auth/[...nextauth]/route.ts
+++ apps/web/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import NextAuth from "next-auth"
 import type { NextAuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
     providers: [
         CredentialsProvider({
             name: 'Credentials',
